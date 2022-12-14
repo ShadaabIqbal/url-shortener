f3fe8dc810aefc0183ea411f5a9a199f91974955
KLChakraborty <chakrabortykajal2@gmail.com>
2022-12-14 13:08:47 +0530

    Done

--- src/urlController/urlController.js
+++ src/urlController/urlController.js
@@ -1,6 +1,27 @@
 const urlModel = require('../urlModel/urlModel')
 const shortid = require('shortid')
 const axios = require('axios')
+const redis = require('redis')
+
+const redisClient = redis.createClient(
+    13190,
+    "redis-13190.c301.ap-south-1-1.ec2.cloud.redislabs.com",
+    { no_ready_check: true }
+  );
+  redisClient.auth("gkiOIPkytPI3ADi14jHMSWkZEo2J5TDG", function (err) {
+    if (err) throw err;
+  });
+  
+  redisClient.on("connect", async function () {
+    console.log("Connected to Redis..");
+  });
+  
+  
+  
+  //2. Prepare the functions for each command
+  
+  const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+  const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 
 const createUrl = async function (req, res) {
 
