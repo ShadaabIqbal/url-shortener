1e2ceeb766b11f28b4a649957998cc03e1d7a2f1
Aman <amanmohadikar111@gmail.com>
2022-12-15 11:42:47 +0530

    setex

--- src/urlController/urlController.js
+++ src/urlController/urlController.js
@@ -8,21 +8,21 @@ const redisClient = redis.createClient(
     13616,
     "redis-13616.c52.us-east-1-4.ec2.cloud.redislabs.com",
     { no_ready_check: true }
-  );
-  redisClient.auth("wRs9ZFCYewsG2rHnLkjGzuZC9hmGSGI4", function (err) {
+);
+redisClient.auth("wRs9ZFCYewsG2rHnLkjGzuZC9hmGSGI4", function (err) {
     if (err) throw err;
-  });
-  
-  redisClient.on("connect", async function () {
+});
+
+redisClient.on("connect", async function () {
     console.log("Connected to Redis..");
-  });
-  
-  
-  
-  //2. Prepare the functions for each command
-  
-  const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
-  const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+});
+
+
+
+//2. Prepare the functions for each command
+
+const SETEX_ASYNC = promisify(redisClient.SETEX).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 
 const createUrl = async function (req, res) {
 
@@ -31,22 +31,22 @@ const createUrl = async function (req, res) {
         if (!Object.keys(req.body).includes('longUrl')) return res.status(400).send({ status: false, message: 'request body should contain only longUrl' })
         const longUrl = req.body.longUrl
         if (typeof longUrl !== 'string' || longUrl == null) return res.status(400).send({ status: false, message: 'Url is not string' })
-       
-                //validation for Url
+
+        //validation for Url
         let correctUrl = false
-       await axios.get(longUrl)
+        await axios.get(longUrl)
             .then(() => { correctUrl = true })
             .catch(() => { correctUrl = false })
         if (correctUrl === false) {
             return res.status(400).send({ status: false, message: "Url is not valid" })
         }
-        
+
 
         let presentURL = await urlModel.findOne({ longUrl: longUrl }).select({ updatedAt: 0, createdAt: 0, __v: 0, _id: 0 })
         if (presentURL) return res.status(200).send({ status: true, data: presentURL })
         let urlCode = shortid.generate().toLowerCase()
         let shortUrl = 'http://localhost:3000' + '/' + urlCode
-        let savedData = await urlModel.create( { longUrl: longUrl, urlCode: urlCode, shortUrl: shortUrl } )
+        let savedData = await urlModel.create({ longUrl: longUrl, urlCode: urlCode, shortUrl: shortUrl })
         let object = { longUrl: savedData.longUrl, urlCode: savedData.urlCode, shortUrl: savedData.shortUrl }
         return res.status(201).send({ status: true, data: object })
     }
@@ -55,18 +55,21 @@ const createUrl = async function (req, res) {
     }
 }
 
+
 const getUrl = async function (req, res) {
     try {
         let urlCode = req.params.urlCode
         let cachedUrl = await GET_ASYNC(`${req.params.urlCode}`)
-        if(cachedUrl) {
+        if (cachedUrl) {
             let parsedData = JSON.parse(cachedUrl)
-         return res.status(302).redirect(parsedData.longUrl)}
-         else{
-        let getData = await urlModel.findOne({ urlCode: urlCode })
-        await SET_ASYNC(`${req.params.urlCode}`, JSON.stringify(getData))
-        if (!getData) return res.status(400).send({ status: false, message: 'no url found' })
-        return res.status(302).redirect(getData.longUrl)}
+            return res.status(302).redirect(parsedData.longUrl)
+        }
+        else {
+            let getData = await urlModel.findOne({ urlCode: urlCode })
+            await SETEX_ASYNC(`${req.params.urlCode}`, 96000, JSON.stringify(getData))
+            if (!getData) return res.status(400).send({ status: false, message: 'no url found' })
+            return res.status(302).redirect(getData.longUrl)
+        }
 
     } catch (error) {
         return res.status(500).send({ status: false, message: error.message })
